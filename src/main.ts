1a597c151b4f6463d4e97ab775e05c849be61c1e
Angel Quiroz Soriano <developer.aqs@gmail.com>
2026-04-04 04:19:09 -0500

    fix: port assign

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  const port = process.env.PORT ?? 3000, '0.0.0.0';
-  await app.listen(port);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application running on port ${port}`);
 }
 bootstrap();
