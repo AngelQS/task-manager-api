38babef7d28437c51c02b8a1d072331279409d1c
AngelQS <developer.aqs@gmail.com>
2026-04-07 11:18:58 -0500

    chore: add logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
 import { TasksModule } from './tasks/tasks.module';
 import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [SupabaseModule, HealthModule, TasksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SupabaseModule,
+    HealthModule,
+    TasksModule,
+  ],
 })
 export class AppModule {}
